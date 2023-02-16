1c18447fd48c961168ec6a9d267f279235f99949
POkosun <POkosun@renmoney.com>
2023-02-16 12:48:18 +0100

    Setup Environment

---
+++ src/test/java/com/seerbit/transstats/TransStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seerbit.transstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
